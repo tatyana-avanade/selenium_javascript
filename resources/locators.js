046cc9af0597864ab5498eccc88d4835c4ec00c7
tatyana-avanade <tatyana.kiseleva@avanade.com>
2023-07-14 12:36:19 -0700

    added Page Object Model example

---
+++ resources/locators.js
@@ -0,0 +1,29 @@
+// locators.js
+// ===========
+
+const locators = {
+    username: "#username",
+    password: "#password",
+    email_field: "#email",
+    submitButton: 'button[type="submit"]',
+    errorMessage: "#flash.error",
+    successMessage:"#flash.success",
+    loginPageHeading: "h2",
+    secureAreaPageHeading: "h2",
+    logoutButton: ".button.secondary.radius",
+  };
+  
+  const data = {
+    baseUrl: "https://the-internet.herokuapp.com/",
+    loginPageUrl: 'login',
+    forgotPasswordUrl: 'forgot_password',
+    pageTitle: "The Internet",
+    username: "tomsmith",
+    password: "SuperSecretPassword!",
+    loginPageHeading: "Login Page",
+    secureAreaPageHeading: "Secure Area",
+    errorMessage: "Your username is invalid!",
+    successMessage: "You logged into a secure area!",
+  };
+  
+  module.exports = { locators, data };
